85df40f16db51fccaa5da696dc7217b3dddb9fe9
SANDE <ndasunpriya.nd@gmail.com>
2024-07-15 03:03:30 +0530

    Task 02 powershell script.

--- Test02/script.ps1
+++ Test02/script.ps1
@@ -1,9 +1,9 @@
-$linuxUser = "username"                         # Replace with Linux username
-$linuxHost = "linuxhostname"                    # Replace with Linux hostname or IP address
-$sshKeyPath = "C:\path\to\private_key.ppk"      # Replace with path to private key file (.ppk)
+$linuxUser = "username"                 # Replace with Linux username
+$linuxHost = "linuxhostname"            # Replace with Linux hostname or IP address
+$sshKeyPath = "C:\path\to\private_key"  # Replace with path to private key file
 
-$plinkCommand = "& 'C:\path\to\plink.exe' -ssh -i $sshKeyPath $linuxUser@$linuxHost 'date'"
+$sshCommand = "ssh -i ""$sshKeyPath"" $linuxUser@$linuxHost 'date'"
 
-$dateTime = Invoke-Expression -Command $plinkCommand
+$dateTime = Invoke-Expression -Command $sshCommand
 
 Write-Host "Date and time on $linuxHost: $dateTime"
