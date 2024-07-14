47bb06e16dcb9cf82013f7c9dc042fba9dfd3ee9
SANDE <ndasunpriya.nd@gmail.com>
2024-07-14 23:49:20 +0530

    Update server.js

--- Test01/backend/server.js
+++ Test01/backend/server.js
@@ -14,21 +14,21 @@ const app = express();
 
 // --------------------------- Middleware -----------------------------
 
-app.use(cors(
-  {
-    origin: "https://dms-electronics-client.vercel.app",
-    methods: ["GET", "POST"],
-    credentials: true, 
-  }
-));
+// app.use(cors(
+//   {
+//     origin: "https://dms-electronics-client.vercel.app",
+//     methods: ["GET", "POST"],
+//     credentials: true, 
+//   }
+// ));
 
 // Enable CORS for specific origin
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://dms-electronics-client.vercel.app');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://dms-electronics-client.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json());
 app.use(morgan("dev"));
