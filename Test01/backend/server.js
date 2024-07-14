9abf4bdd302bfd667a800b5e6ddd2199dbcd48a8
SANDE <ndasunpriya.nd@gmail.com>
2024-07-14 23:34:39 +0530

    Update vercel.json

--- Test01/backend/server.js
+++ Test01/backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // app.use(cors(
 //   {
-//     origin: "*",
+//     origin: "https://dms-electronics-client.vercel.app",
 //     methods: ["GET", "POST"],
 //     credentials: true, 
 //   }
