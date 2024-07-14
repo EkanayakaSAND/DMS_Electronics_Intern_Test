d949a130e0be61d7205e637c463e54f950d846c5
SANDE <ndasunpriya.nd@gmail.com>
2024-07-14 22:16:26 +0530

    Cors

--- Test01/backend/server.js
+++ Test01/backend/server.js
@@ -14,7 +14,13 @@ const app = express();
 
 // --------------------------- Middleware -----------------------------
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use(morgan("dev"));
 dbConnection();
